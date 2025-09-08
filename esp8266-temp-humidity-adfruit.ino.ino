a8f14c9cdc593d3cea4436ee3e7eb6b2d1eb0cee
Ting <zhuting100@163.com>
2025-09-08 15:34:31 +0800

    Init.

---
+++ esp8266-temp-humidity-adfruit.ino.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Include the DHT11 library for interfacing with the sensor.
+#include <DHT11.h>
+
+// Create an instance of the DHT11 class.
+// - For Arduino: Connect the sensor to Digital I/O Pin 2.
+// - For ESP32: Connect the sensor to pin GPIO2 or P2.
+// - For ESP8266: Connect the sensor to GPIO2 or D4.
+DHT11 dht11(2);
+
+// WiFi settings
+const char *ssid = "";       // Replace with your WiFi name
+const char *password = "";   // Replace with your WiFi password
+
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+#define AIO_FEED        ""
+
+// MQTT Broker settings
+const char *mqtt_broker = "io.adafruit.com"; // Broker Host
+const char *mqtt_topic = "";                 // MQTT topic
+const int mqtt_port = 1883;                  // MQTT port (TCP)
+
+WiFiClient espClient;
+PubSubClient mqtt_client(espClient);
+
+void connectToWiFi();
+
+void connectToMQTTBroker();
+
+void mqttCallback(char *topic, byte *payload, unsigned int length);
+
+void setup() {
+    Serial.begin(115200);
+    connectToWiFi();
+    mqtt_client.setServer(mqtt_broker, mqtt_port);
+}
+
+void loop() {
+    if (!mqtt_client.connected()) {
+        connectToMQTTBroker();
+    }
+
+    int temperature = 0;
+    int humidity = 0;
+    char payload[100];
+
+    // Attempt to read the temperature and humidity values from the DHT11 sensor.
+    int result = dht11.readTemperatureHumidity(temperature, humidity);
+
+    if(result == 0){
+        sprintf(payload, "Temperatur: %d °C, Humididy:%d %%", temperature, humidity);
+        mqtt_client.publish(mqtt_topic, payload);
+        Serial.print(payload);
+        Serial.print("\n");
+
+    } else{
+        // Print error message based on the error code.
+        Serial.println(DHT11::getErrorString(result));
+    }
+
+    delay(1000);
+}
+
+void connectToWiFi() {
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to the WiFi network");
+}
+
+void connectToMQTTBroker() {
+    while (!mqtt_client.connected()) {
+        String client_id = "esp8266-client-" + String(WiFi.macAddress());
+        Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
+        if (mqtt_client.connect(client_id.c_str(), AIO_USERNAME, AIO_KEY)) {
+            Serial.println("Connected to MQTT broker");
+        } else {
+            Serial.print("Failed to connect to MQTT broker, rc=");
+            Serial.print(mqtt_client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
